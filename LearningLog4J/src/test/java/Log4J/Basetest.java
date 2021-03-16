c6be1506c4392c80f6c0e9a1c67092736769f880
Sanzid Rashid <sanzidrashid@sanzids-mbp.lan>
2021-03-15 20:18:04 -0400

    Add project

---
+++ LearningLog4J/src/test/java/Log4J/Basetest.java
@@ -0,0 +1,28 @@
+package Log4J;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Basetest {
+	
+	Logger log = Logger.getLogger(Basetest.class);
+	
+	@Before
+	public void start() {
+		DOMConfigurator.configure("log4j.xml");
+	}
+	
+	@Test
+	public void test1() {
+		
+		log.fatal("The is FATAL");
+		log.error("this is error");
+		log.warn("this is warn");
+		log.info("This is info");
+		
+		
+	}
+
+}
